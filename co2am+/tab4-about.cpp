a9a2bb252b07bed858d19e2f7a185cf40c38dcd4
Mikhail Polyanskiy <mpolyanskiy@gmail.com>
2019-12-27 07:48:09 -0500

    Update tab4-about.cpp

--- co2am+/tab4-about.cpp
+++ co2am+/tab4-about.cpp
@@ -2,7 +2,7 @@
 
 void MainWindow::SetAboutText()
 {  
-    QString co2amplus_version = "2019-11-19";
+    QString co2amplus_version = "2019-11-20";
 
     // get co2amp version string
     process = new QProcess(this);
